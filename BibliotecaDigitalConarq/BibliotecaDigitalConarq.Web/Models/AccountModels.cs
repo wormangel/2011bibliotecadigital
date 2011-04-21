2f20a453d7bd3a012dec202ab9c8bea78c04ac43
otaciliolacerda <otaciliolacerda@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-04-21 21:23:53 +0000

    Teste

--- BibliotecaDigitalConarq/BibliotecaDigitalConarq.Web/Models/AccountModels.cs
+++ BibliotecaDigitalConarq/BibliotecaDigitalConarq.Web/Models/AccountModels.cs
@@ -61,7 +61,7 @@ namespace BibliotecaDigitalConarq.Web.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match. ")]
         public string ConfirmPassword { get; set; }
     }
 }
