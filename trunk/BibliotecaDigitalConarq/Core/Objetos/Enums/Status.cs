61e3f0635bb04a57baa364cd620ae66a59026078
wormangel <wormangel@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-04-22 04:18:51 +0000

    Criadas classes referentes a volumes, documentos, etc (WIP)

---
+++ trunk/BibliotecaDigitalConarq/Core/Objetos/Enums/Status.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Core.Objetos.Enums
+{
+    public enum Status
+    {
+        [Description("Minuta/Rascunho (pré-original)")]
+        PreOriginal,
+
+        [Description("Original")]
+        Original,
+
+        [Description("Cópia")]
+        Copia
+    }
+}
---
+++ trunk/BibliotecaDigitalConarq/Core/Objetos/Volume.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Objetos
+{
+    public class Volume
+    {
+        // Metadados 1.6 - Identificador do volume
+        public long Id { get; set; }
+
+        // Referência
+        // Metadados 1.4 - Identificador do processo/dossiê
+        public long IdProcessoDossie
+        {
+            get { return DocumentoArquivistico.Id; }
+        }
+
+        // Referência
+        // Metadados 1.5 - Identificador do processo/dossiê
+        public string NumeroDoProcessoOuDossie
+        {
+            get { return DocumentoArquivistico.NumeroDoProcessoOuDossie; }
+        }
+
+        // Metadados 1.7 - Número do volume
+        public string NumeroDoVolume { get; set; }
+
+
+
+
+        // Referências
+
+        // O processo/dossiê a que este volume pertence
+        public DocumentoArquivistico DocumentoArquivistico { get; set; }
+
+        // Os documentos que este volume contém
+        public IEnumerable<Documento> Documentos { get; set; }
+
+    }
+}
