e85eb43e108777ce99bdf5ce292dbae6f824b16f
wormangel <wormangel@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-04-23 06:09:03 +0000

    Esboço inicial dos gerenciadores de Docs e Acesso a dados

---
+++ trunk/BibliotecaDigitalConarq/Core/Gerenciadores/GerenciadorDocumentosArquivisticos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.Objetos;
+
+namespace Core.Gerenciadores
+{
+    class GerenciadorDocumentosArquivisticos
+    {
+        private GerenciadorAcessoADados AcessoADados = new GerenciadorAcessoADados(); 
+
+        public void CriaDocumento(Documento doc)
+        {
+            AcessoADados.SalvaVersaoDeDocumentoArquivistico(doc);
+        }
+    }
+}
