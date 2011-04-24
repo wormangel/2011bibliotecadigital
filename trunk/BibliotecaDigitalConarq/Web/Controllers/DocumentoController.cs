47ea3626334579622847742c8d8e47285b2478fd
vitor88 <vitor88@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-04-24 23:55:35 +0000

    Criado os controllers e views dos documentos arquivísticos, volumes e documentos.

---
+++ trunk/BibliotecaDigitalConarq/Web/Controllers/DocumentoController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Objetos;
+using Web.Models;
+
+namespace Web.Controllers
+{ 
+    public class DocumentoController : Controller
+    {
+        private GerenciadorDocumentos servico = new GerenciadorDocumentos();
+
+        //
+        // GET: /Documento/
+
+        public ViewResult Index()
+        {
+            IEnumerable<Documento> documentos = servico.RecuperarDocumentos();
+            return View(documentos);
+        }
+
+        //
+        // GET: /Documento/Details/5
+
+        public ViewResult Details(long id)
+        {
+            return View(servico.RecuperarPorId(id));
+        }
+
+        //
+        // GET: /Documento/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Documento/Create
+
+        [HttpPost]
+        public ActionResult Create(Documento documento)
+        {
+            if (ModelState.IsValid)
+            {
+                servico.Salvar(documento);
+                return RedirectToAction("Index");  
+            }
+
+            return View(documento);
+        }
+        
+        //
+        // GET: /Documento/Edit/5
+ 
+        public ActionResult Edit(long id)
+        {
+            return View(servico.RecuperarPorId(id));
+        }
+
+        //
+        // POST: /Documento/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Documento documento)
+        {
+            if (ModelState.IsValid)
+            {
+                servico.Salvar(documento);
+                return RedirectToAction("Index");
+            }
+            return View(documento);
+        }
+
+        //
+        // GET: /Documento/Delete/5
+ 
+        public ActionResult Delete(long id)
+        {
+            return View(servico.RecuperarPorId(id));
+        }
+
+        //
+        // POST: /Documento/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            servico.Remover(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ trunk/BibliotecaDigitalConarq/Web/Controllers/VolumeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Objetos;
+using Web.Models;
+
+namespace Web.Controllers
+{ 
+    public class VolumeController : Controller
+    {
+        private GerenciadorVolumes servico = new GerenciadorVolumes();
+
+        //
+        // GET: /Volume/
+
+        public ViewResult Index()
+        {
+            IEnumerable<Volume> volumes = servico.RecuperarVolumes();
+            return View(volumes);
+        }
+
+        //
+        // GET: /Volume/Details/5
+
+        public ViewResult Details(long id)
+        {
+            return View(servico.RecuperarPorId(id));
+        }
+
+        //
+        // GET: /Volume/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Volume/Create
+
+        [HttpPost]
+        public ActionResult Create(Volume volume)
+        {
+            if (ModelState.IsValid)
+            {
+                servico.Salvar(volume);
+                return RedirectToAction("Index");  
+            }
+
+            return View(volume);
+        }
+        
+        //
+        // GET: /Volume/Edit/5
+ 
+        public ActionResult Edit(long id)
+        {
+            // mesma coisa do details, ver se tem como reaproveitar algo (DRY!)
+            return View(servico.RecuperarPorId(id));
+        }
+
+        //
+        // POST: /Volume/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Volume volume)
+        {
+            if (ModelState.IsValid)
+            {
+                servico.Salvar(volume);
+                return RedirectToAction("Index");
+            }
+            return View(volume);
+        }
+
+        //
+        // GET: /Volume/Delete/5
+ 
+        public ActionResult Delete(long id)
+        {
+            // mesma coisa do details, ver se tem como reaproveitar algo (DRY!)
+            return View(servico.RecuperarPorId(id));
+        }
+
+        //
+        // POST: /Volume/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            servico.Remover(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
