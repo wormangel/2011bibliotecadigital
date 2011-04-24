8741e9177929cdfdb136dc00ab74408b0c249d9b
vitor88 <vitor88@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-04-24 23:55:35 +0000

    Criado os controllers e views dos documentos arquivísticos, volumes e documentos.

---
+++ BibliotecaDigitalConarq/Web/Controllers/DocumentoArquivisticoController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Gerenciadores;
+using Core.Objetos;
+using Web.Models;
+
+namespace Web.Controllers
+{ 
+    public class DocumentoArquivisticoController : Controller
+    {
+        private GerenciadorDocumentosArquivisticos servico = new GerenciadorDocumentosArquivisticos();
+
+        //
+        // GET: /DocumentoArquivistico/
+
+        public ViewResult Index()
+        {
+            IEnumerable<DocumentoArquivistico> documentos = servico.RecuperarDocumentos();
+            return View(documentos);
+        }
+
+        //
+        // GET: /DocumentoArquivistico/Details/5
+
+        public ViewResult Details(long id)
+        {
+            return View(servico.RecuperarPorId(id));
+        }
+
+        //
+        // GET: /DocumentoArquivistico/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /DocumentoArquivistico/Create
+
+        [HttpPost]
+        public ActionResult Create(DocumentoArquivistico documentoArquivistico)
+        {
+            if (ModelState.IsValid)
+            {
+                servico.Salvar(documentoArquivistico);
+                return RedirectToAction("Index");  
+            }
+
+            return View(documentoArquivistico);
+        }
+        
+        //
+        // GET: /DocumentoArquivistico/Edit/5
+ 
+        public ActionResult Edit(long id)
+        {
+            // mesma coisa do details, ver se tem como reaproveitar algo (DRY!)
+            return View(servico.RecuperarPorId(id));
+        }
+
+        //
+        // POST: /DocumentoArquivistico/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(DocumentoArquivistico documentoarquivistico)
+        {
+            if (ModelState.IsValid)
+            {
+                servico.Salvar(documentoarquivistico);
+                return RedirectToAction("Index");
+            }
+            return View(documentoarquivistico);
+        }
+
+        //
+        // GET: /DocumentoArquivistico/Delete/5
+ 
+        public ActionResult Delete(long id)
+        {
+            // mesma coisa do details, ver se tem como reaproveitar algo (DRY!)
+            return View(servico.RecuperarPorId(id));
+        }
+
+        //
+        // POST: /DocumentoArquivistico/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            servico.Remover(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
