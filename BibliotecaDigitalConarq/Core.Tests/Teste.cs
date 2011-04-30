1c5a68e6bb77c0513ccb8e7ee42ad706cef1ab28
otaciliolacerda <otaciliolacerda@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-04-30 22:22:42 +0000

    Organizei o projeto de testes.

--- BibliotecaDigitalConarq/Core.Tests/Teste.cs
+++ BibliotecaDigitalConarq/Core.Tests/Teste.cs
@@ -1,19 +1,32 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Gerenciadores;
+using Core.Interfaces;
+using Core.Objetos;
+using NUnit.Framework;
+using NUnit.Mocks;
 
 namespace Core.Tests
 {
     [TestFixture]
-    public class ExemploTests
+    public class GerenciadorDocumentosArquivisticosTests
     {
         [Test]
-        public void Exemplo_Teste()
+        public void Tenta_Pegar_Todos_Os_Documentos_Arquivisticos()
         {
             // Arrange
-            
-            // Act
-            
-            // Assert
-            
-        }   
+            List<DocumentoArquivistico> documentos = new List<DocumentoArquivistico> { new DocumentoArquivistico(), new DocumentoArquivistico() };
+            var repositorioMock = new DynamicMock(typeof(IRepositorio<DocumentoArquivistico>));
+
+            // Diz ao mock que quando for chamado o método "PegaTodos" retornar
+            // a lista de documentos definidos.
+            repositorioMock.ExpectAndReturn("PegarTodos", documentos);
+
+            // Instancia o gerenciador com o repositório mock
+            GerenciadorDocumentosArquivisticos servico = new GerenciadorDocumentosArquivisticos((IRepositorio<DocumentoArquivistico>)repositorioMock.MockInstance);
+
+            // Act e Assert
+            documentos.DeveSerIgualA(servico.RecuperarDocumentos());
+        }
     }
 }
--- BibliotecaDigitalConarq/Core/Gerenciadores/GerenciadorDocumentosArquivisticos.cs
+++ BibliotecaDigitalConarq/Core/Gerenciadores/GerenciadorDocumentosArquivisticos.cs
@@ -1,22 +1,25 @@
-﻿using Core.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Interfaces;
 using Core.Objetos;
-using Ninject;
 
 namespace Core.Gerenciadores
 {
     public class GerenciadorDocumentosArquivisticos
     {
-        private readonly Repositorio<DocumentoArquivistico> AcessoADados = new Repositorio<DocumentoArquivistico>(); 
-        
-        [Inject]
-        private readonly ILogger logger;
+        private readonly IRepositorio<DocumentoArquivistico> AcessoADados;
+
+        public GerenciadorDocumentosArquivisticos(IRepositorio<DocumentoArquivistico> repositorio)
+        {
+             AcessoADados = repositorio; 
+        }
         
         public void CriaDocumento(DocumentoArquivistico doc)
         {
             // Verifica com o gerenciador de segurança..
             // Indexa com o gerenciador de indexação.. (inclusive arquivos se houver)
             // Loga com o gerenciador de logging..
-
 
             AcessoADados.Adicionar(doc);
             //logger.LogaAcaoDocumentoArquivistico(doc.Id, usuario, "Criado processo/dossiê");
@@ -25,6 +28,12 @@ namespace Core.Gerenciadores
         public void AtualizaDocumento(DocumentoArquivistico doc)
         {
             AcessoADados.Editar(doc);
+        }
+
+        public List<DocumentoArquivistico> RecuperarDocumentos()
+        {
+            // TODO fazer.
+            throw new NotImplementedException();
         }
     }
 }
