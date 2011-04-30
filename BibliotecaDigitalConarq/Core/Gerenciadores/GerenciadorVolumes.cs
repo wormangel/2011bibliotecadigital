25bd1717659fee258e3458f99e28ab0a702bad38
otaciliolacerda <otaciliolacerda@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-04-30 23:29:53 +0000

    Resolvi diversos problemas de referência e métodos que não existiam (criei o stub), pois estou tentando fazer os testes mas ta dando problema porque o projeto não esta buildando.

---
+++ BibliotecaDigitalConarq/Core/Gerenciadores/GerenciadorVolumes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Interfaces;
+using Core.Objetos;
+
+namespace Core.Gerenciadores
+{
+    public class GerenciadorVolumes
+    {
+        public GerenciadorVolumes(IRepositorio<Volume> repositorio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Volume> RecuperarVolumes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Volume RecuperarPorId(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Salvar(Volume volume)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
