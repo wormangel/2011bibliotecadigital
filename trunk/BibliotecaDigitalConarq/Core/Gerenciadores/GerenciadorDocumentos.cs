0105f940093a679311986541bb205330c7f05f2e
vitor88 <vitor88@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-05-01 20:16:40 +0000

    Captura de upload de arquivo no controller implementado. Criado gerenciador de documentos para eliminar erro de importação no controller.

---
+++ trunk/BibliotecaDigitalConarq/Core/Gerenciadores/GerenciadorDocumentos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.Interfaces;
+using Core.Objetos;
+
+namespace Core.Gerenciadores
+{
+    public class GerenciadorDocumentos
+    {
+        private readonly IRepositorio<Documento> repositorio;
+
+        public GerenciadorDocumentos(IRepositorio<Documento> repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public void salvarArquivo(long id, HttpPostedFileBase arquivo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Documento> RecuperarDocumentos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Documento RecuperarPorId(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Salvar(Documento documento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Adicionar(Documento documento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
