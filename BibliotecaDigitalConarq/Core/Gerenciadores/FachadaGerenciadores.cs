7dc0d4e6f8c9e2c2e1777356e90a8abc213bdf51
vitor88 <vitor88@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-05-07 20:18:59 +0000

    Added fachada para os controllers.

---
+++ BibliotecaDigitalConarq/Core/Gerenciadores/FachadaGerenciadores.cs
@@ -0,0 +1,22 @@
+﻿using Core.Gerenciadores;
+using Core.Interfaces;
+using Core.Objetos;
+
+namespace Core.Gerenciadores
+{
+    public class FachadaGerenciadores
+    {
+        private readonly GerenciadorDocumentosArquivisticos _documentosArquivisticos; 
+        private readonly GerenciadorVolumes _volumes;
+        private readonly GerenciadorDocumentos _documentos;
+
+        public FachadaGerenciadores(IRepositorio<DocumentoArquivistico> repositorioDocumentosArquivisticos, IRepositorio<Volume> repositorioVolumes, IRepositorio<Documento> repositorioDocumentos)
+        {
+            this._documentosArquivisticos = new GerenciadorDocumentosArquivisticos(repositorioDocumentosArquivisticos);
+            this._volumes = new GerenciadorVolumes(repositorioVolumes);
+            this._documentos = new GerenciadorDocumentos(repositorioDocumentos);
+        }
+
+        // TODO: implementar métodos que serão chamados pelos controllers.
+    }
+}
