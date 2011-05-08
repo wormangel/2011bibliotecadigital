1a44b7eff1e5a5e47d678ac76a44143b3e761fb0
otaciliolacerda <otaciliolacerda@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-05-08 19:23:23 +0000

    Mandando os arquivos que faltaram: packages.config e NinjectMVC3.cs

--- trunk/BibliotecaDigitalConarq/Core.Tests/TesteCRUDGerenciadorDocumentoArquivistico.cs
+++ trunk/BibliotecaDigitalConarq/Core.Tests/TesteCRUDGerenciadorDocumentoArquivistico.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Core.Gerenciadores;
 using Core.Interfaces;
 using Core.Objetos;
@@ -12,120 +10,126 @@ namespace Core.Tests
     [TestFixture]
     public class TesteCrudGerenciadorDocumentosArquivisticos
     {
-        private GerenciadorDocumentosArquivisticos _gerenciador;
-        private DynamicMock _repositorioMock;
+        #region Setup/Teardown
 
         [SetUp]
         public void SetUp()
         {
-            this._repositorioMock = new DynamicMock(typeof(IRepositorio<DocumentoArquivistico>));
-            this._gerenciador = new GerenciadorDocumentosArquivisticos((IRepositorio<DocumentoArquivistico>) this._repositorioMock.MockInstance);
+            _repositorioMock = new DynamicMock(typeof (IRepositorio<DocumentoArquivistico>));
+            _gerenciador =
+                new GerenciadorDocumentosArquivisticos(
+                    (IRepositorio<DocumentoArquivistico>) _repositorioMock.MockInstance);
+        }
+
+        #endregion
+
+        private GerenciadorDocumentosArquivisticos _gerenciador;
+        private DynamicMock _repositorioMock;
+
+        [Test]
+        public void AtualizarDocumento()
+        {
+            var documento = new DocumentoArquivistico {Id = 1, Descricao = "Descrição1"};
+
+            _repositorioMock.Call("Adicionar", documento);
+            _repositorioMock.ExpectAndReturn("RecuperarPorId", documento, 1);
+            _repositorioMock.Call("Salvar", documento);
+            _repositorioMock.ExpectAndReturn("RecuperarPorId", documento, 1);
+
+            _gerenciador.Criar(documento);
+            DocumentoArquivistico docTemp = _gerenciador.RecuperarPorId(1);
+            Assert.AreEqual("Descrição1", docTemp.Descricao);
+            docTemp.Descricao = "Descrição2";
+            _gerenciador.Atualizar(docTemp);
+            DocumentoArquivistico docTemp2 = _gerenciador.RecuperarPorId(1);
+            Assert.AreEqual("Descrição2", docTemp2.Descricao);
+
+            _repositorioMock.Verify();
         }
 
         [Test]
         public void CriarDocumento()
         {
             var documento = new DocumentoArquivistico();
-            var documentosRecuperados = new List<DocumentoArquivistico> { documento };
-            
-            this._repositorioMock.Call("Adicionar", documento);
-            this._repositorioMock.ExpectAndReturn("RecuperarTodos", documentosRecuperados);
+            var documentosRecuperados = new List<DocumentoArquivistico> {documento};
 
-            this._gerenciador.Criar(documento);
-            Assert.AreEqual(documentosRecuperados, this._gerenciador.RecuperarDocumentos());
-            
-            this._repositorioMock.Verify();
+            _repositorioMock.Call("Adicionar", documento);
+            _repositorioMock.ExpectAndReturn("RecuperarTodos", documentosRecuperados);
+
+            _gerenciador.Criar(documento);
+            Assert.AreEqual(documentosRecuperados, _gerenciador.RecuperarDocumentos());
+
+            _repositorioMock.Verify();
         }
 
         [Test]
         public void RecuperarDocumentoPorId()
         {
             var documento = new DocumentoArquivistico {Id = 1, Descricao = "Descrição"};
-            var documentosRecuperados = new List<DocumentoArquivistico> { documento };
+            var documentosRecuperados = new List<DocumentoArquivistico> {documento};
 
-            this._repositorioMock.Call("Adicionar", documento);
-            this._repositorioMock.ExpectAndReturn("RecuperarPorId", documento, 1);
-            this._repositorioMock.ExpectAndReturn("RecuperarTodos", documentosRecuperados);
+            _repositorioMock.Call("Adicionar", documento);
+            _repositorioMock.ExpectAndReturn("RecuperarPorId", documento, 1);
+            _repositorioMock.ExpectAndReturn("RecuperarTodos", documentosRecuperados);
 
-            this._gerenciador.Criar(documento);
-            var docTemp = this._gerenciador.RecuperarPorId(1);
-            Assert.AreEqual(documentosRecuperados, this._gerenciador.RecuperarDocumentos());
+            _gerenciador.Criar(documento);
+            DocumentoArquivistico docTemp = _gerenciador.RecuperarPorId(1);
+            Assert.AreEqual(documentosRecuperados, _gerenciador.RecuperarDocumentos());
             Assert.AreEqual("Descrição", docTemp.Descricao);
 
-            this._repositorioMock.Verify();
+            _repositorioMock.Verify();
         }
 
         [Test]
         public void RecuperarDocumentoPorIdInexistente()
         {
-            this._repositorioMock.ExpectAndReturn("RecuperarPorId", null, 1);
+            _repositorioMock.ExpectAndReturn("RecuperarPorId", null, 1);
 
             // devemos ignorar ou lançar exceção dizendo que o id não existia?
-            Assert.Null(this._gerenciador.RecuperarPorId(1));
-                
-            this._repositorioMock.Verify();    
+            Assert.Null(_gerenciador.RecuperarPorId(1));
+
+            _repositorioMock.Verify();
+        }
+
+        [Test]
+        public void RecuperarDocumentosArquivisticos()
+        {
+            var documentosRecuperados = new List<DocumentoArquivistico>
+                                            {new DocumentoArquivistico(), new DocumentoArquivistico()};
+
+            _repositorioMock.ExpectAndReturn("RecuperarTodos", documentosRecuperados);
+
+            Assert.AreEqual(documentosRecuperados, _gerenciador.RecuperarDocumentos());
+
+            _repositorioMock.Verify();
         }
 
         [Test]
         public void RemoverDocumento()
         {
-            var documento = new DocumentoArquivistico { Id = 1, Descricao = "Descrição" };
+            var documento = new DocumentoArquivistico {Id = 1, Descricao = "Descrição"};
 
-            this._repositorioMock.ExpectAndReturn("RecuperarPorId", documento, 1);
-            this._repositorioMock.Call("Remover", 1);
-            this._repositorioMock.ExpectAndReturn("RecuperarPorId", null, 1);
+            _repositorioMock.ExpectAndReturn("RecuperarPorId", documento, 1);
+            _repositorioMock.Call("Remover", 1);
+            _repositorioMock.ExpectAndReturn("RecuperarPorId", null, 1);
 
-            var docTemp = this._gerenciador.RecuperarPorId(1);
+            DocumentoArquivistico docTemp = _gerenciador.RecuperarPorId(1);
             Assert.AreEqual("Descrição", docTemp.Descricao);
-            this._gerenciador.Remover(1);
-            Assert.Null(this._gerenciador.RecuperarPorId(1));
+            _gerenciador.Remover(1);
+            Assert.Null(_gerenciador.RecuperarPorId(1));
 
-            this._repositorioMock.Verify();
+            _repositorioMock.Verify();
         }
 
         [Test]
         public void RemoverDocumentoInexistente()
         {
-            this._repositorioMock.Call("Remover", 1);
-            
+            _repositorioMock.Call("Remover", 1);
+
             // devemos ignorar ou lançar exceção dizendo que o id não existia?
-            this._gerenciador.Remover(1);
+            _gerenciador.Remover(1);
 
-            this._repositorioMock.Verify();
+            _repositorioMock.Verify();
         }
-
-        [Test]
-        public void AtualizarDocumento()
-        {
-            var documento = new DocumentoArquivistico { Id = 1, Descricao = "Descrição1"};
-
-            this._repositorioMock.Call("Adicionar", documento);
-            this._repositorioMock.ExpectAndReturn("RecuperarPorId", documento, 1);
-            this._repositorioMock.Call("Salvar", documento);
-            this._repositorioMock.ExpectAndReturn("RecuperarPorId", documento, 1);
-
-            this._gerenciador.Criar(documento);
-            var docTemp = this._gerenciador.RecuperarPorId(1);
-            Assert.AreEqual("Descrição1", docTemp.Descricao);
-            docTemp.Descricao = "Descrição2";
-            this._gerenciador.Atualizar(docTemp);
-            var docTemp2 = this._gerenciador.RecuperarPorId(1);
-            Assert.AreEqual("Descrição2", docTemp2.Descricao);
-
-            this._repositorioMock.Verify();
-        }
-        
-        [Test]
-        public void RecuperarDocumentosArquivisticos()
-        {
-            var documentosRecuperados = new List<DocumentoArquivistico> { new DocumentoArquivistico(), new DocumentoArquivistico() };
-
-            this._repositorioMock.ExpectAndReturn("RecuperarTodos", documentosRecuperados);
-
-            Assert.AreEqual(documentosRecuperados, this._gerenciador.RecuperarDocumentos());
-            
-            this._repositorioMock.Verify();
-        }
-
     }
 }
