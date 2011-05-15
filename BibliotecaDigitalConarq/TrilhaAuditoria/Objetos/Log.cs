3bb37bf111de2431ea619dd52b8fa1f6d354c6b9
otaciliolacerda <otaciliolacerda@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-05-15 18:55:58 +0000

    Alterei a visibilidade para poder ser acessível de outros projetos. Camda de acesso a dados esta desorganizada.

--- BibliotecaDigitalConarq/TrilhaAuditoria/Objetos/Log.cs
+++ BibliotecaDigitalConarq/TrilhaAuditoria/Objetos/Log.cs
@@ -3,13 +3,13 @@ using TrilhaAuditoria.Enums;
 
 namespace TrilhaAuditoria.Objetos
 {
-    internal class Log
+    public class Log
     {
-        private TipoDoLog Tipo { get; set; }
-        private long Id { get; set; }
-        private long Usuario { get; set; }
-        private String Acao { get; set; }
-        private DateTime Data { get; set; }
+        public TipoDoLog Tipo { get; set; }
+        public long Id { get; set; }
+        public long Usuario { get; set; }
+        public String Acao { get; set; }
+        public DateTime Data { get; set; }
 
         public Log(TipoDoLog tipo, long id, long usuario, String acao)
         {
