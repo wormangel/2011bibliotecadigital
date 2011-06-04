27421e9545618215d94f659cbb110ac6ab8e58fc
vitor88 <vitor88@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-06-04 20:55:13 +0000

    Adicionado entidades para plano de classificação

---
+++ BibliotecaDigitalConarq/Core/Objetos/Classificacao/Classificacao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Core.Objetos.Classificacao
+{
+    public class Classificacao
+    {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Codigo { get; set; }
+
+        public Boolean Ativa { get; set; }
+
+        [Display(Name = "Registros a respeito da classe")]
+        public virtual ICollection<RegistroClasse> RegistrosClasse { get; set; }
+
+        public virtual ICollection<DocumentoArquivistico> DocumentosArquivisticos { get; set; }
+}
---
+++ BibliotecaDigitalConarq/Core/Objetos/Classificacao/Grupo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Objetos.Classificacao
+{
+    class Grupo : Classificacao
+    {
+        public long IdSubclasse { get { return (Subclasse != null) ? Subclasse.Id : -1; }  }
+
+        [Display(Name = "Subclasse que contém este grupo")]
+        public virtual Subclasse Subclasse { get; set; }
+
+        [Display(Name = "Subgrupos contidos neste grupo")]
+        public virtual ICollection<Subgrupo> Subgrupos { get; set; }
+    }
+}
