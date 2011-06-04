0e5e79946022c29ad8af8d3166aa53f57344ea31
vitor88 <vitor88@bcee5d84-eb41-b6d3-f15b-48af2247b092>
2011-06-04 20:55:13 +0000

    Adicionado entidades para plano de classificação

---
+++ trunk/BibliotecaDigitalConarq/Core/Objetos/Classificacao/RegistroClasse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Core.Objetos.Classificacao
+{
+    public class RegistroClasse
+    {
+
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Display(Name = "Subclasse que contém este grupo")]
+        public virtual Classificacao Subclasse { get; set; }
+
+        public TipoRegistroClasse TipoRegistro { get; set;}
+
+        public String Responsavel { get; set; }
+    }
+
+    public internal enum TipoRegistroClasse
+    {
+        [Description("Abertura")]
+        Abertura,
+
+        [Description("Desativação")]
+        Desativacao,
+
+        [Description("Reativação")]
+        Reativacao,
+
+        [Description("Mudança de nome")]
+        MudancaNome,
+
+        [Description("Deslocamento")]
+        Deslocamento,
+
+        [Description("Extinção")]
+        Extincao
+    }
+}
---
+++ trunk/BibliotecaDigitalConarq/Core/Objetos/Classificacao/Subclasse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Objetos.Classificacao
+{
+    class Subclasse : Classificacao
+    {
+        public long IdClasse { get { return (Classe != null) ? Classe.Id : -1; } }
+
+        [Display(Name = "Classe que contém esta subclasse")]
+        public virtual Classe Classe { get; set; }
+
+        [Display(Name = "Grupos contidos nesta subclasse")]
+        public virtual ICollection<Grupo> Grupos { get; set; }
+    }
+}
